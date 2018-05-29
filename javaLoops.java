fe84052ab39729ebe4dc97958345b73167c51741
Conde <mouzclik@users.noreply.github.com>
2018-05-29 14:03:49 -0400

    Create javaLoops.java

---
+++ javaLoops.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author acond003
+ */
+public class WLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String out = "";
+        while (true) {
+            String words = JOptionPane.showInputDialog("Enter a word. Click cancel to quit");
+            if (words == null) {
+                break;
+            }
+            if (words.equals("Alabama")) {
+                continue;
+            }
+            out = out + words + "\n";
+        }//End While
+        JTextArea displayArea1 = new JTextArea(out, 15, 30);
+        JOptionPane.showMessageDialog(null, displayArea1);
+        int sum = 0;
+        
+        while (true) {
+            String output = JOptionPane.showInputDialog("Enter a Word and a Number");
+            if (output == null) {
+                break;
+            }
+            Scanner Scan = new Scanner(output);
+            String words = Scan.next();
+            int B = Scan.nextInt();
+            out = out + words;
+            sum = sum + B;
+        }
+        out = out + sum;
+        JTextArea displayArea2 = new JTextArea(out, 15, 30);
+        JOptionPane.showMessageDialog(null, displayArea2);
+    }
+}
